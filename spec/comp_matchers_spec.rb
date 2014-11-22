b538a395eab2474bfc0210160d199f94d22447b1
RJ Robinson <robertj.robinson@gmail.com>
2014-11-22 08:23:49 -0500

    Complex matchers

---
+++ spec/comp_matchers_spec.rb
@@ -0,0 +1,37 @@
+describe 'Comp matchers' do
+  it 'works with nested data' do
+
+    class Product
+
+      def initialize id, name, category
+        @id =id
+        @name = name
+        @category = category
+      end
+      def serial_data
+        [
+          "X-",
+          @name[0],
+          @category[0],
+          @id
+        ]
+      end
+
+    end   # of class
+
+    product = Product.new 2456, 'Tomato', 'Fruit'
+
+    expect(product.serial_data).to match [
+      a_string_starting_with('X-'),
+      a_string_starting_with("T"),
+      a_string_starting_with("F"),
+      a_value < 5000
+    ] # X-TF2456
+
+    # change
+    # contain_exactly
+    # include
+    # yield_with_args
+
+  end     # of block
+end
